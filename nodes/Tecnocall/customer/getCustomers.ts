b299ef2cfc11aa41942ddefa92ffa58f75ac9e81
Mahdi Khakdaman <Khakdamanmohammadmahdi@gmail.com>
2025-10-24 00:01:41 +0330

    fix: correct API endpoint for createCustomer and getCustomers functions

--- nodes/Tecnocall/customer/getCustomers.ts
+++ nodes/Tecnocall/customer/getCustomers.ts
@@ -3,12 +3,7 @@ import type { ILoadOptionsFunctions, INodeParameters } from 'n8n-workflow';
 export async function getCustomers(this: ILoadOptionsFunctions): Promise<INodeParameters> {
 	const response = await this.helpers.httpRequest.call(this, {
 		method: 'GET',
-		baseURL: '={{$credentials?.baseUrl}}',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-		},
-		url: 'api/bots/customers',
+		url: 'api/bot/customers',
 	});
 	return {
 		results: response.data,
