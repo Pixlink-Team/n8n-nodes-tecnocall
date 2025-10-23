79556f813e0d96eb90b04d3caf130f45b0595d0b
Mahdi Khakdaman <Khakdamanmohammadmahdi@gmail.com>
2025-10-23 23:55:41 +0330

    fix: add headers and baseURL to getCustomers function for API request

--- nodes/Tecnocall/customer/getCustomers.ts
+++ nodes/Tecnocall/customer/getCustomers.ts
@@ -3,6 +3,11 @@ import type { ILoadOptionsFunctions, INodeParameters } from 'n8n-workflow';
 export async function getCustomers(this: ILoadOptionsFunctions): Promise<INodeParameters> {
 	const response = await this.helpers.httpRequest.call(this, {
 		method: 'GET',
+		baseURL: '={{$credentials?.baseUrl}}',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+		},
 		url: 'api/bots/customers',
 	});
 	return {
